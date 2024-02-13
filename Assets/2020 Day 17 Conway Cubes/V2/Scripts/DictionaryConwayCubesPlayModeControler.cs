145cf9db1a91f935306d89f26dc7cf996c00a791
Templar <carlegos101@gmail.com>
2024-02-13 02:34:43 -0600

    Added Range to Timer and Made Default Colors Nicer :)

--- Assets/2020 Day 17 Conway Cubes/V2/Scripts/DictionaryConwayCubesPlayModeControler.cs
+++ Assets/2020 Day 17 Conway Cubes/V2/Scripts/DictionaryConwayCubesPlayModeControler.cs
@@ -6,7 +6,7 @@ public class DictionaryConwayCubesPlayModeControler : MonoBehaviour
 {
     public DictionaryConwayCubes dictionaryConwayCubes;
     public bool RunTimer = false;
-    public float MaxTimer = 3;
+    [Range(0,1)]public float MaxTimer = 3;
     public float tempTimer = 0;
     private void OnValidate()
     {
